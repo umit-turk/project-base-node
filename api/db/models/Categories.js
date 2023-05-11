681bb65601a44f467790b5636356e3f6d0ae9572
Umit Turk <uyt.turk@gmail.com>
2023-05-12 02:12:45 +0300

    add category and custom response

--- api/db/models/Categories.js
+++ api/db/models/Categories.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema(
   {
     is_active: { type: Boolean, default: true },
-    created_by: { type: mongoose.SchemaTypes.ObjectId, required: true },
+    created_by: { type: mongoose.SchemaTypes.ObjectId},
+    name: { type: String, required: true },
   },
   {
     timestamps: {
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Categories = require("../db/models/Categories");
+const Response = require("../lib/Response");
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({success:true});
+/*GET categories */
+router.get("/", async (req, res, next) => {
+  try {
+    let categories = await Categories.find({});
+    res.json(Response.successResponse(categories));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(err)
+    res.status(errorResponse.code).json(errorResponse)
+  }
 });
+
+//POST CATEGORIES
+router.post("/add", async (req, res) => {
+  let body = req.body
+  try {
+    if(!body.name) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST,"Validation Error!","name field must be filled")
+    let cateory = new Categories({
+      name:body.name,
+      is_active:true,
+      created_by:req.user?.id
+    });
+
+    await cateory.save();
+
+    res.json(Response.successResponse({success:true}))
+
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error)
+    res.status(errorResponse.code).json(errorResponse)
+  }
+})
 
 module.exports = router;
